05a69a4b5b540c1366d1c3f5930027733102852c
brcelso <celso.sjunior@outlook.com>
2023-04-23 19:39:10 -0300

    k

--- app/page.tsx
+++ app/page.tsx
@@ -122,11 +122,9 @@ export default function IndexPage() {
       <ExamplesNav className="[&>a:first-child]:text-primary" />
       
 
-      <section className="hidden md:block">
         <div className="overflow-hidden rounded-lg border bg-background shadow-xl">
         <TradingViewWidget />
         </div>
-      </section>
 
 
         {/*<section className="space-y-8 overflow-hidden rounded-lg border-2 border-primary dark:border-muted md:hidden">
